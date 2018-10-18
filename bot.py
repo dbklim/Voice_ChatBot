642f6183007cae22156360e25690f90c25d9eebf
Vlad <vladislav@Vladislav.Ubuntu>
2018-10-18 19:22:14 +0300

    Add learning results with different parameters and training samples

--- bot.py
+++ bot.py
@@ -14,8 +14,7 @@ import signal
 import platform
 import curses
 import os
-
-# Отредактировать диалоги пьесы, обучить заново и протестировать. Затем добавить распознавание голоса с микрофона и ответ голосом.
+import time
 
 f_source_data = 'data/source_data.txt'
 f_prepared_data = 'data/prepared_data.txt'
@@ -25,20 +24,57 @@ f_w2v_model = 'data/w2v_model.bin'
 f_w2v_vocab = 'data/w2v_vocabulary.txt'
 f_w2v_nbhd = 'data/w2v_neighborhood.txt'
 f_net_model = 'data/net_model.txt'
-f_net_weights = 'data/net_50_weights.h5'
+f_net_weights = 'data/net_final_weights.h5'
 
 def train():
+    start_time = time.time()
     prep = Preparation()
     prep.prepare_all(f_source_data, f_prepared_data)
 
     w2v = CoderW2V()
-    w2v.words2vec(f_prepared_data_pkl, f_encoded_data, f_w2v_model, f_w2v_vocab, f_w2v_nbhd, size = 150, epochs = 100) # 500 1000 для авторских данных
+    w2v.words2vec(f_prepared_data_pkl, f_encoded_data, f_w2v_model, f_w2v_vocab, f_w2v_nbhd, size = 500, epochs = 1000)
 
     t = Training()
-    t.train(f_encoded_data, f_net_model, 2, 50, 5)
+    t.train(f_encoded_data, f_net_model, 2, 50, 10)   
 
     pr = Prediction(f_net_model, f_net_weights, f_w2v_model)
     pr.assessment_training_accuracy(f_encoded_data)
+
+    print('[i] Общее время обучения: %.4f мин или %.4f ч' % ((time.time() - start_time)/60.0, ((time.time() - start_time)/60.0)/60.0))
+    
+    # 248 входных фраз, epochs = 100
+    # 10 итераций, 5 эпох - точность 0% (size = 150)
+    # 50 итераций, 5 эпох - точность 22.58% (size = 150)
+    # 50 итераций, 10 эпох - точность 61.29% (size = 150)
+    # 50 итераций, 10 эпох - точность 69.35% (size = 200)
+
+    # 50 итераций, 15 эпох - точность 64.52% (size = 100)
+    # 50 итераций, 15 эпох - точность 83.87% (size = 150)
+    # 50 итераций, 15 эпох - точность 80.65% (size = 200)
+
+    # 50 итераций, 20 эпох - точность 95.16% (size = 150)
+    # 50 итераций, 20 эпох - точность 98.39% (size = 200) (244 из 248 правильных ответов)
+
+    # 50 итераций, 25 эпох - точность 96.77% (size = 150)
+    # 50 итераций, 25 эпох - точность 99% (size = 200) (246 из 248 правильных ответов)
+
+    # 50 итераций, 30 эпох - точность 96.77% (size = 150) (240 из 248 правильных ответов)
+    # 50 итераций, 30 эпох - точность 100% (size = 200) (248 из 248 правильных ответов)
+
+    # 1596 входных фраз, epochs = 1000, 50 итераций по 30 эпох
+    # size = 1250 - недостаточно видеопамяти (ошибка аллокации)
+    # size = 1100 - недостаточно видеопамяти
+    # size = 1000 - недостаточно видеопамяти
+    # size = 900 - обучение займёт примерно 44.5 часов
+    # size = 800 - обучение займёт примерно 33 часа
+    # size = 700 - обучение займёт примерно 25.8 часа
+    # size = 600 - обучение займёт примерно 21.2 часа
+    # size = 500 - обучение займёт примерно 15.4 часа
+
+    # 1596 входных фраз, epochs = 1000, size = 500
+    # 50 итераций, 20 эпох - обучение займёт примерно 9.4 часа
+    # 50 итераций, 15 эпох - обучение займёт примерно 7.3 часа
+    # 50 итераций, 10 эпох - точность 98.62% (1574 из 1596 правильных ответов), время обучения - 4.8ч или 287.7 мин
 
 def predict():
     pr = Prediction(f_net_model, f_net_weights, f_w2v_model)
