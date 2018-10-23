5fe175eecb88d29dc165c2b87554e0be6b5f2b68
Vlad <vladislav@Vladislav.Ubuntu>
2018-10-23 15:58:25 +0300

    Bug fixes

--- text_to_speech.py
+++ text_to_speech.py
@@ -24,34 +24,38 @@ def tts(text, mode, filename_audio = None):
     3. filename_audio - имя .wav файла для сохранения синтезированной речи в режиме into_file (если не задан, используется /data/answer.wav) '''
 
     if filename_audio == None:
-        filename_audio = '/data/answer.wav'
+        filename_audio = 'data/answer.wav'
     # Запись синтезированной речи в .wav файл с частотой дискретизации 32 кГц и глубиной 16 бит, моно, используется sox
     command_line = "echo '" + text + "' | RHVoice-client -s Anna+CLB " 
-    command_line += "| sox -t wav - -r 32000 -c 1 -b 16 -t wav - >'" + os.path.dirname(sys.argv[0]) + filename_audio + "'"
+    command_line += "| sox -t wav - -r 32000 -c 1 -b 16 -t wav - >'" + os.path.dirname(os.path.realpath(__file__)) + '/' + filename_audio + "'"
     if mode == 'playback':
         subprocess.call(command_line, shell=True)
-        sound = AudioSegment.from_wav(os.path.dirname(sys.argv[0]) + filename_audio)
+        sound = AudioSegment.from_wav(os.path.dirname(os.path.realpath(__file__)) + '/' + filename_audio)
         play(sound)
         # command_line += "| aplay" - не используется, т.к. при каждом обращении выводится сообщение от самого RHVoice в терминал
     elif mode == 'into_file':
         subprocess.call(command_line, shell=True)
     else:
-        print('[E] Неподдерживаемый режим работы, проверьте правильность передаваемых параметров.')
+        print('[E] Неподдерживаемый режим работы, проверьте значение переменной mode.')
         return
      
     # Если вылазит FileNotFoundError: [Errno 2] No such file or directory: 'ffprobe', нужно выполнить apt-get install ffmpeg libavcodec-extra
 
 
+
+
 def main():
-    tts('я вас не понимаю, извините', 'into_file', '/data/answer.wav')
+    tts('ты мне  друг или нет?', 'into_file', 'data/answer.wav')
 
 
 if __name__ == '__main__':
     main()
         
-# В данном варианте нужно как-то ждать завершения воспроизведения, т.к. выполнения основного потока не прерывается.
-# Тоже самое при использовании pygame.mixer
-# import pyglet
-# explosion = pyglet.media.load('/home/vladislav/Проекты/test.wav', streaming=False)
-# explosion.play()
-# pyglet.app.run()
+
+''' 
+В данном варианте нужно как-то ждать завершения воспроизведения, т.к. выполнения основного потока не прерывается.
+Тоже самое при использовании pygame.mixer
+import pyglet
+explosion = pyglet.media.load('/home/vladislav/Проекты/test.wav', streaming=False)
+explosion.play()
+pyglet.app.run() '''
