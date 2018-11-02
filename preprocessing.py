ac46726df9309d58cbfe6c176e11581f7ac3e45c
Vlad <vladislav@Vladislav.Ubuntu>
2018-11-02 15:45:47 +0300

    Added selection of questions from training sample for RESTful-server

--- preprocessing.py
+++ preprocessing.py
@@ -40,11 +40,11 @@ class Preparation:
         self.input_size = number_words.max() + 2  # количество входов сети
 
         '''
-        Что бы посмотреть предложения, которые длиннее 25 слов
+        # Что бы посмотреть предложения, которые длиннее 25 слов
         for words in data:
-            if len(words[0]) > 25:
+            if len(words[0]) > 23:
                 print('0: %s || %i' % (words[0], len(words[0])))
-            if len(words[1]) > 25:
+            if len(words[1]) > 23:
                 print('1: %s || %i' % (words[1], len(words[1])))
                 '''
 
@@ -53,6 +53,7 @@ class Preparation:
         print('[i] Сохранение результата в %s и %s.pkl' % (filename_out, filename_out[:filename_out.find('.')]))
         self.__data_write(data, filename_out) 
         self.__data_info(number_words)
+
 
     def prepare_quest(self, quest):
         ''' Преобразует вопрос к сети в последовательность фиксированного размера.
@@ -66,6 +67,7 @@ class Preparation:
         quest = self.__fill_cells_quest(quest)    
         return quest
 
+
     def prepare_answer(self, answer):
         ''' Преобразует ответ сети в виде последовательности фиксированного размера в предложение.
         1. answer - последовательность фиксированного размера
@@ -77,15 +79,36 @@ class Preparation:
         answer = ' '.join([ w for w in answer[0:i] if (w != '<PAD>') and (w != '<EOS>') and (w != '<GO>') ])       
         return self.__prepare_answer(answer)
 
+
+    def get_questions(self, filename_in):
+        ''' Загрузка из файла текста в виде списка строк "вопрос %% ответ" и выделение вопросов.
+        1. filename_in - имя входного .txt файла с исходными данными
+        2. возвращает список вопросов '''
+        with open(filename_in, 'r') as file: 
+            content = file.readlines()
+
+        # удаление пробелов в начале и конце каждой строки "вопрос %% ответ" и разбиение каждой строки на две части
+        content = [ str.strip() for str in content ]
+        content = [ str.split('%%') for str in content ] 
+
+        result = []
+        for x in content: # для всех пар [вопрос, ответ]
+            q = x[0].strip() # удаление лишних пробелов
+            if len(q) > 0:
+                result.append(q) # сохранение вопросов
+        return result
+
+
     def __prepare_answer(self, answer):
         ''' Очистка ответа от повторений знаков препинания и замена букв в нижнем регистре после знаков препинания '!', '?' и '.' 
         на буквы в верхнем регистре. '''
 
-        # Удаление пробелов перед ',', '.', '!' и '?'
+        # Удаление пробелов перед ',', '.', '!', '?' и '…'
         answer = re.sub(r'\s,', ',', answer)
         answer = re.sub(r'\s\.', '.', answer)
         answer = re.sub(r'\s!', '!', answer)
         answer = re.sub(r'\s\?', '?', answer)
+        answer = re.sub(r'\s…', '…', answer)
         # Замена нескольких подряд идущих ','  '!' и '?' на одиночные
         answer = re.sub(r',{2,5}', ',', answer)
         answer = re.sub(r'!{2,5}', '!', answer)
@@ -143,13 +166,15 @@ class Preparation:
             i += 1
         return result
 
+
     def __data_clean(self, data):
-        ''' Очистка всех пар [вопрос, ответ] от неподдерживаемых символов. '''
+        ''' Очистка всех пар [вопрос, ответ] от знаков препинания и неподдерживаемых символов. '''
         result = [ [ self.__clean_quest(q), self.__clean_answer(a) ] for [q,a] in data ]
         return result
 
+
     def __clean_quest(self, quest):
-        ''' Очистка вопроса от неподдерживаемых символов. '''
+        ''' Очистка вопроса от знаков препинания и неподдерживаемых символов. '''
         quest = quest.lower()
         quest = re.sub(r'\.', '', quest) 
         quest = re.sub(r',', '', quest) 
@@ -171,8 +196,9 @@ class Preparation:
         #quest = re.sub(r'[\W]+', '', quest) # удаление всех не букв
         return quest
     
+
     def __clean_answer(self, answer):
-        ''' Очистка ответа от неподдерживаемых символов. '''
+        ''' Очистка ответа от лишних знаков препинания и неподдерживаемых символов. '''
         answer = answer.lower()
         #answer = re.sub(r'\.', '', answer) 
         #answer = re.sub(r',', '', answer) 
@@ -193,6 +219,7 @@ class Preparation:
         #answer = re.sub(r'[\W]+', '', answer) # удаление всех не букв
         return answer
 
+
     def __data_split(self, data):
         ''' Разбиение пар [вопрос, ответ] или одиночного вопроса на отдельные слова (знаки препинания - отдельные элементы). '''
         if isinstance(data, str): # если полученный объект - одиночный вопрос
@@ -203,6 +230,7 @@ class Preparation:
             result = [ [ [ w for w in reversed(q) ], a ] for [q,a] in result ] # перестраивание слов вопроса в обратном порядке
         return result
     
+    
     def __tokenizer(self, str):
         ''' Разбиение строки на слова. '''
         result = re.split(r'(\W)', str) # разбиение строки на последовательность из слов и знаков препинания
@@ -212,20 +240,24 @@ class Preparation:
                 del result[-1]
         return result
 
+
     def __fill_cells(self, data):
         ''' Выравнивание всех пар по размеру. '''
         result = [ [self.__fill_cells_quest(q), self.__fill_cells_answ(a)] for [q,a] in data ]
         return result
+
 
     def __fill_cells_answ(self, a):
         ''' Выравнивание ответа по размеру, заполняя пустые места словом <PAD>. Например: [..., '<EOS>', '<PAD>', ...] '''
         result = a + ['<EOS>'] + ['<PAD>'] * (self.input_size - len(a) - 1) 
         return result
 
+
     def __fill_cells_quest(self, q):
         ''' Выравнивание вопроса по размеру, заполняя пустые места словом <PAD>. Например: [..., '<PAD>', 'вопрос', '<GO>'] '''
         result = ['<PAD>'] * (self.input_size - len(q) - 1) + q + ['<GO>']
         return result
+
 
     def __data_write(self, data, filename):
         ''' Запись полученных данных в .txt и .pkl файлы. '''
@@ -235,6 +267,7 @@ class Preparation:
         with open(filename, 'w') as file:
             for d in data:
                 print(d, file=file)
+
 
     def __data_info(self, number_words):
         ''' Вывод информации о полученных парах [вопрос, ответ]. '''
@@ -258,7 +291,13 @@ class Preparation:
 
 def main():
     b = Preparation()
-    b.prepare_all('data/source_data.txt', 'data/prepared_data.txt')
+    #b.prepare_all('data/source_data.txt', 'data/prepared_data.txt')
+    a = b.get_questions('data/source_data.txt')
+
+    c = {'questions':a}
+    print(c.get('questions')[1600])
+    print()
+
 
 if __name__ == '__main__':
     main()
